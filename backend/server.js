4d00d7135104f8714ada91c2a1de5db0f8368f9f
d-invisible <108665598+d-invisible@users.noreply.github.com>
2024-10-30 06:14:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://placeholder.com",
+    origin: "https://real-time-chatapp-backend2.onrender.com",
     // credentials: true,
   },
 });
